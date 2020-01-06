d9595675a118bb0a1f920ee527b331fb70c656cd
DBuchinger@hoopercorp.com <DEREKBU@hoopercorp.com>
2020-01-06 09:01:42 -0600

    added comments and removed test function

--- ColorPuzzle/ColorPuzzle.js
+++ ColorPuzzle/ColorPuzzle.js
@@ -1,3 +1,4 @@
+// Making each Square object and putting them in an array to cycle through.
 var A1 = new Square("A1", 0, 1, 1);
 var A2 = new Square("A2", 0, 2, 1);
 var A3 = new Square("A3", 0, 3, 1);
@@ -16,6 +17,7 @@ var D3 = new Square("D3", 0, 3, 4);
 var D4 = new Square("D4", 0, 4, 4);
 var grid = [A1, A2, A3, A4, B1, B2, B3, B4, C1, C2, C3, C4, D1, D2, D3, D4];
 
+// Square constructor
 function Square(name, count, xCoord, yCoord) {
     this.name = name;
     this.count = count;
@@ -23,6 +25,8 @@ function Square(name, count, xCoord, yCoord) {
     this.yCoord = yCoord;
 }
 
+// the main function. It changes the color of any clicked button and all of its
+// neighbors.
 function colorChange(id) {
     var target = document.getElementById(id);
     var selected = matchSquare(id);
@@ -72,6 +76,7 @@ function colorChange(id) {
     }
 }
 
+// the function for matching a clicked button with its Square object
 function matchSquare(id) {
     for (i = 0; i < grid.length; i++) {
         if (grid[i].name == id) {
@@ -80,13 +85,10 @@ function matchSquare(id) {
     }
 }
 
+// resets the grid back to its original (white) configuration
 function reset() {
     for (i = 0; i < grid.length; i++) {
         document.getElementById(grid[i].name).style.background = 'linear-gradient(#ffffff, #d9d9d9)';
         grid[i].count = 0;
     }
-}
-
-function testFunc() {
-    window.alert(grid[0].name);
 }
